afd7424f1a26c5dfff31fd8683d42b4207b3653e
pmita <panos.mita2@outlook.com>
2021-05-07 14:05:37 +0100

    logic added

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-//js code goes here
+//Global selection
+const generateColorBtn = document.querySelector('.btn');
+const backgroundContainer = document.querySelector('.background-container');
+const colorText = document.querySelector('.color-text');
 
-console.log('it works');
+const allHexCharacter = [0, 1 ,2 ,3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+//Event Listneres
+generateColorBtn.addEventListener('click', function(){
+    let generatedHex = '#';
+    for( let i=0; i< 6; i++){
+        generatedHex += allHexCharacter[randomIndex()];
+    }
+    backgroundContainer.style.backgroundColor = generatedHex;
+    colorText.textContent = generatedHex;
+});
+
+function randomIndex() {
+    return Math.floor((Math.random() * allHexCharacter.length));
+}
